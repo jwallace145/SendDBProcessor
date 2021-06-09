ec18d7c821fc201dc5ab3f768729a3eaaaddb728
James  Wallace <jameswallace@Jamess-MBP.lan1>
2021-06-09 00:04:13 -0400

    fixed routes processor constructor bug

--- clients/routes_processor.py
+++ clients/routes_processor.py
@@ -1,7 +1,10 @@
+from clients.dynamodb_client import DynamoDBClient
+
+
 class RoutesProcessor:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dynamodb_client: DynamoDBClient) -> None:
+        self.dynamodb_client = dynamodb_client
 
     def process(self, payload) -> None:
         return 0
